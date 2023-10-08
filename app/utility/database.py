dba3135554ed8d5a99779fb5d6993d522f2c2457
Mark Achiles <markachilesflores2004@gmail.com>
2023-10-08 18:33:47 +0800

    Fix
    - Change database access from needing an access key to utilizing API.
    
    To-do:
    - Add a token to make sure that this is only usable by authorized people.

--- app/utility/database.py
+++ app/utility/database.py
@@ -1,16 +1,29 @@
 import random
-import boto3
-from boto3.dynamodb.conditions import Key
+import requests
 
-session = boto3.Session(profile_name="awscc-backend", region_name="ap-southeast-1")
-dynamodb = session.resource("dynamodb")
-table = dynamodb.Table("awscc_members")
+
+def get_id(email: str) -> str:
+    api = f"https://fe80kk63kd.execute-api.ap-southeast-1.amazonaws.com/get_id/{email}"
+    request = requests.get(url=api)
+    clubId = request.json()["member"]
+
+    if clubId == "None":
+        print(f"{email} is not registered in the database.")
+
+    return clubId
 
 
 def get_nickname(email: str) -> str:
-    response = table.query(IndexName="email-index", KeyConditionExpression=Key("email").eq(email))
-    return random.choice(response["Items"][0]["nicknames"])
+    clubId = get_id(email)
+    if clubId == "None":
+        return "AWStronaut"
+
+    api = f"https://fe80kk63kd.execute-api.ap-southeast-1.amazonaws.com/members/{clubId}"
+    request = requests.get(url=api)
+    member = request.json()["member"]
+    nickname = random.choice(member["nicknames"])
+    return nickname
 
 
 if __name__ == "__main__":
-    print(get_nickname("markachilesflores2004@gmail.com"))
+    print(get_nickname("machibytes@gmail.com"))
